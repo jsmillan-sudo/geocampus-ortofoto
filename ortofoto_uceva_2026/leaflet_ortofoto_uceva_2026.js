b044163cfe3564051876dbdbff44aff8bd7120c8
jsmillan-sudo <jsmillan@uceva.edu.co>
2026-07-03 00:49:40 -0500

    Borré Otofoto 2

--- ortofoto_uceva_2026/leaflet_ortofoto_uceva_2026.js
+++
@@ -1,9 +0,0 @@
-const boundsOrtofotoUceva2026 = [[4.061115447267966, -76.20701537414645], [4.067673752489288, -76.19705661330963]];
-
-const ortofotoUceva2026 = L.tileLayer('https://jsmillan-sudo.github.io/geocampus-ortofoto/ortofoto_uceva_2026/{z}/{x}/{y}.png', {
-  minZoom: 16,
-  maxZoom: 21,
-  bounds: boundsOrtofotoUceva2026,
-  tms: false,
-  attribution: 'Ortofoto UCEVA 2026'
-});
